bcbdf70fe1d9b64add3029548aa26925bbd5b8f5
daniela <danniela.alejandra@gmail.com>
2025-01-20 14:04:54 -0300

    Subiendo archivos

---
+++ assets/js/index.js
@@ -0,0 +1,26 @@
+
+let img1 = document.querySelector(".nutria")
+img1.style.borderRadius = '30px'
+img1.style.border = 'none'
+
+
+img1.addEventListener("click", () => {
+  if (img1.style.border == 'none')
+    (img1.style.border = '2px solid  #ff0000');
+  else (img1.style.border = 'none' )
+});
+
+
+
+const select1 = document.querySelector('#primero')
+const select2 = document.querySelector('#segundo')
+const select3 = document.querySelector('#tercero')
+const btn = document.querySelector('.btn')
+const input = document.querySelector('input')
+
+
+btn.addEventListener ('click', () => {
+  
+    
+})
+
